24b53ede33a5353b78ada99a0a6b2a4f4ce414e6
Alexandre Langlais <alxlanglais@gmail.com>
2017-03-08 15:07:55 +0100

    Profile model updated to correct has_one

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,5 @@
 class Profile < ApplicationRecord
-  has_one :user
+  belongs_to :user
 
   validates :profile_type, presence: true, inclusion: { in: ["client","artist"], allow_nil: false }
   validates :firstname, presence: true
