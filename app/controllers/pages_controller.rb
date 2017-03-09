f28d7fe7d851f96e8eb4684532cf9dee423efa0a
Alexandre Langlais <alxlanglais@gmail.com>
2017-03-09 16:02:30 +0100

    show all artists ok #17

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
+    @profiles = Profile.all.select { |profile| profile.profile_type == "artist"}
   end
 end
