e9ccd540abdac1b9ae94a2b07ee7343279f2a62e
Alexandre Langlais <alxlanglais@gmail.com>
2017-03-13 18:08:44 +0100

    Orders#index done as table

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :profiles, only: [:show, :edit, :update] do
-    resources :orders, only: [:new, :create, :edit, :update, :destroy]
+    resources :orders, only: [:index, :new, :create, :edit, :update, :destroy]
   end
 
   root to: 'pages#index'
