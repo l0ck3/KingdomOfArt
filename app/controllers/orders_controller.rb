cde27fa19cc7c32dc9fd925ec5672185602b487e
Alexandre Langlais <alxlanglais@gmail.com>
2017-03-13 15:25:22 +0100

    Method New and Create done #19

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,50 @@
+class OrdersController < ApplicationController
+  before_action :find_artist_profile, only: [:new, :edit, :destroy, :update]
+  layout "order_new", only: [ :new ]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @order = Order.new
+    @client_user = User.find(current_user)
+    @artist_user = User.find(@artist_profile.user)
+    @product = Product.where(user_id: @artist_user.id) # Change with a real link to the selected product !
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.artist = Profile.find(params[:profile_id]).user
+    @order.user = current_user
+    if @order.save
+    else
+      fail # need to manage errors
+    end
+    redirect_to profile_path(params[:profile_id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_artist_profile
+    @artist_profile = Profile.find(params[:profile_id])
+  end
+
+  def order_params
+    # *Strong params*: You need to *whitelist* what
+    # can be updated by the user
+    # Never trust user data!
+    params.require(:order).permit(:status, :product_id, :offer_title, :offer_body, :offer_price)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-
   ActiveAdmin.routes(self)
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :profiles, only: [:show, :edit, :update]
+  resources :profiles, only: [:show, :edit, :update] do
+    resources :orders, only: [:new, :create, :edit, :update, :destroy]
+  end
 
   root to: 'pages#index'
-
-
 end
