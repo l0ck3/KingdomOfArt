1ad09437169b064dcefbedad206911a9bfc3a6ef
Langlais Alexandre <alxlanglais@gmail.com>
2017-03-16 09:17:38 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-<<<<<<< HEAD
-  resources :profiles, only: [:show, :edit, :update]
-  resources :products, only: [:show, :new, :create, :edit, :update, :destroy]
-=======
   resources :profiles, only: [:show, :edit, :update] do
     resources :orders, only: [:index, :new, :create, :edit, :update, :destroy]
   end
->>>>>>> 57e1d223fb80a18c0d853b9ba05ca71a633cf5e2
-
+  resources :products, only: [:show, :new, :create, :edit, :update, :destroy]
   root to: 'pages#index'
 end
